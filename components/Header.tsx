1efa941638b6a0c2a952f43c0a2355421d5b9cd0
Avazkhon <avazkhon1996@gmail.com>
2023-09-27 17:00:07 +0500

    Header and Nav added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,41 @@
-// components/Header.tsx
-import React from "react";
+import React from 'react';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
+import Nav from "@/components/Nav";
 
-const Header: React.FC = () => {
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "400", "600", "700"],
+});
+
+export default function Header() {
   return (
     <header className="bg-blue-500 p-4 text-white">
-      <nav className="flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
         <div>
           <h1 className="text-2xl font-bold">Udemy Clone</h1>
         </div>
-        <div>
-          {/* Add navigation links or user authentication here */}
+        <div className="space-x-4">
+          <a href="#" className="hover:underline">
+            Home
+          </a>
+          <a href="#" className="hover:underline">
+            Courses
+          </a>
+          <a href="#" className="hover:underline">
+            Categories
+          </a>
+          <a href="#" className="hover:underline">
+            About Us
+          </a>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
 
-export default Header;
