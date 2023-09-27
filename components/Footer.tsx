f070d3f8d932a24f705e9aaf7a91f66e1ec21d00
Avazkhon <avazkhon1996@gmail.com>
2023-09-27 16:50:32 +0500

    Footer added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,74 @@
-// components/Header.tsx
-import React from "react";
+import Link from "next/link";
 
-const Header: React.FC = () => {
+export default function Footer() {
   return (
-    <header className="bg-blue-500 p-4 text-white">
-      <nav className="flex justify-between items-center">
-        <div>
-          <h1 className="text-2xl font-bold">Udemy Clone</h1>
-        </div>
-        <div>
-          {/* Add navigation links or user authentication here */}
-        </div>
-      </nav>
-    </header>
+<footer className="bg-gray-900 text-gray-300 py-10">
+  <div className="container mx-auto px-4">
+    <div className="flex flex-wrap">
+      <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6 sm:mb-0">
+        <h2 className="text-xl font-semibold mb-4">Categories</h2>
+        <ul>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Technology</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Business</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Design</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Health</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Science</a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6 sm:mb-0">
+        <h2 className="text-xl font-semibold mb-4">Help</h2>
+        <ul>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Support</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">FAQ</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6 sm:mb-0">
+        <h2 className="text-xl font-semibold mb-4">Legal</h2>
+        <ul>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Privacy Policy</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Terms of Use</a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6 sm:mb-0">
+        <h2 className="text-xl font-semibold mb-4">Social</h2>
+        <ul>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Facebook</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Twitter</a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="hover:text-white">Instagram</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="mt-8 text-center sm:text-left">
+      <p>&copy; 2023 Udenote. All rights reserved.</p>
+    </div>
+  </div>
+</footer>
   );
-};
-
-export default Header;
+}
